18e0ef685c9d5e56ca5f6133f0cff33f0fc4c2f1
hugo.hamon <hugo.hamon@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-08-07 09:05:49 -0500

    [sfYahooGeocoderPlugin] added unit tests for array access implementation
    git-svn-id: http://svn.symfony-project.com/plugins/sfYahooGeocoderPlugin/trunk@20895 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- test/unit/response/sfYahooGeocoderResponseXmlTest.php
+++ test/unit/response/sfYahooGeocoderResponseXmlTest.php
@@ -9,7 +9,7 @@ try
 {
   $response = new sfYahooGeocoderResponseXml();
   
-  $t = new lime_test(17, new lime_output_color());
+  $t = new lime_test(25, new lime_output_color());
 }
 catch (Exception $e)
 {
@@ -30,38 +30,46 @@ $t->diag('->getLatitude()');
 
 $t->isa_ok($response->getLatitude(), 'double', '->getLatitude() returns a double value');
 $t->is($response->getLatitude(), 48.870480, '->getLatitude() returns "48.870480"');
+$t->is($response['Latitude'], 48.870480, '->offsetGet() returns "48.870480"');
 
 $t->diag('->getLongitude()');
 
 $t->isa_ok($response->getLongitude(), 'double', '->getLongitude() returns a double value');
 $t->is($response->getLongitude(), 2.305420, '->getLongitude() returns "2.305420"');
+$t->is($response['Longitude'], 2.305420, '->offsetGet() returns "2.305420"');
 
 $t->diag('->getCountry()');
 
 $t->isa_ok($response->getCountry(), 'string', '->getCountry() returns a string value');
 $t->is($response->getCountry(), 'FR', '->getCountry() returns "FR"');
+$t->is($response['Country'], 'FR', '->offsetGet() returns "FR"');
 
 $t->diag('->getState()');
 
 $t->isa_ok($response->getState(), 'string', '->getState() returns a string value');
 $t->is($response->getState(), 'France', '->getState() returns "France"');
+$t->is($response['State'], 'France', '->offsetGet() returns "France"');
 
 $t->diag('->getZip()');
 
 $t->isa_ok($response->getZip(), 'string', '->getZip() returns a string value');
 $t->is($response->getZip(), '75008', '->getZip() returns "75008"');
+$t->is($response['Zip'], '75008', '->offsetGet() returns "75008"');
 
 $t->diag('->getCity()');
 
 $t->isa_ok($response->getCity(), 'string', '->getCity() returns a string value');
 $t->is($response->getCity(), 'Paris', '->getCity() returns "Paris"');
+$t->is($response['City'], 'Paris', '->offsetGet() returns "Paris"');
 
 $t->diag('->getAddress()');
 
 $t->isa_ok($response->getAddress(), 'string', '->getAddress() returns a string value');
 $t->is($response->getAddress(), 'Avenue des Champs-Élysées', '->getAddress() returns "Avenue des Champs-Élysées"');
+$t->is($response['Address'], 'Avenue des Champs-Élysées', '->offsetGet() returns "Avenue des Champs-Élysées"');
 
 $t->diag('->getPrecisionLevel()');
 
 $t->isa_ok($response->getPrecisionLevel(), 'string', '->getPrecisionLevel() returns a string value');
 $t->is($response->getPrecisionLevel(), 'address', '->getPrecisionLevel() returns "address"');
+$t->is($response['PrecisionLevel'], 'address', '->offsetGet() returns "address"');
